f44c5e3379ba8e97750483badb46cb65286dfe35
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-01-02 20:45:21 +0300

    init handler layer

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/SofiyaAndreyeva/cart-api/internal/service"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) InitialRoutes() http.Handler {
+	mux := http.NewServeMux()
+	return mux
+}
