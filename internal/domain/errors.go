907b470e74b3dd7f97ec3644985dbec866b09373
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-01-05 16:30:00 +0300

    add new errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,5 +3,10 @@ package domain
 import "errors"
 
 var (
-	ErrServerRun = errors.New("failed to run http server")
+	ErrServerRun        = errors.New("failed to run http server")
+	ErrLimitCart        = errors.New("cart limit reached")
+	ErrCartNotFound     = errors.New("cart not found")
+	ErrEmptyProduct     = errors.New("empty product")
+	ErrInvalidPrice     = errors.New("invalid price")
+	ErrCartItemNotFound = errors.New("cart item not found")
 )
