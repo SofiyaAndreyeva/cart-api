1578c63bb073bedfc25a3e6e3df8ea006be0319a
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-01-05 13:17:02 +0300

    add cart interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,13 +1,22 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/SofiyaAndreyeva/cart-api/internal/domain"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
+type Cart interface {
+	CreateCart(ctx context.Context) (domain.Cart, error)
+}
 type Repository struct {
+	Cart
 }
 
 func NewRepository(db *sqlx.DB) *Repository {
-	return &Repository{}
+	return &Repository{
+		Cart: NewCartRepository(db),
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,10 +1,22 @@
 package service
 
-import "github.com/SofiyaAndreyeva/cart-api/internal/repository"
+import (
+	"context"
+
+	"github.com/SofiyaAndreyeva/cart-api/internal/domain"
+	"github.com/SofiyaAndreyeva/cart-api/internal/repository"
+)
+
+type Cart interface {
+	CreateCart(ctx context.Context) (domain.Cart, error)
+}
 
 type Service struct {
+	Cart
 }
 
 func NewService(repo *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Cart: NewCartService(repo),
+	}
 }
