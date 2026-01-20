f2497d804018ed553229b89af2c55fe2bdd8ca9d
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2026-01-20 14:23:17 +0300

    add service method to get cart price

--- internal/service/service.go
+++ internal/service/service.go
@@ -12,6 +12,7 @@ type Cart interface {
 	AddToCart(ctx context.Context, cartID int, product string, price float64) (domain.CartItem, error)
 	DeleteFromCart(ctx context.Context, cartID int, cartItemID int) error
 	GetCartItems(ctx context.Context, cartID int) (domain.Cart, error)
+	GetCartPrice(ctx context.Context, cartID int) (domain.CartPriceResponse, error)
 }
 
 type Service struct {
